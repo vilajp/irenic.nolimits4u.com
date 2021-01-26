e6f2c9058131689ddd8aa53ce141a8149e76cca5
vilajp <vilajp@gmail.com>
2021-01-26 00:07:54 -0300

    se modifico la forma en que toma los datos de la lista identificando precio may y min

--- listas.py
+++ listas.py
@@ -12,19 +12,28 @@ def main():
 	sheet = wb_obj.active
 	print(sheet)
 	lista = {}
+	may = "Mayorista"
+	min = "Minorista"
 	nro = 0
 
 	for row in sheet.iter_rows():
+		
 		for cell in row:
 			if (nro%3)==0:
 				articulo = cell.value
-				lista[articulo] = []
-				nro +=1  
-				continue
-			lista[articulo].append(cell.value)
-			nro += 1
+				lista[articulo] = {}
+				 
+				
+			elif (nro%3)==1 and cell.value != None:
+				lista[articulo][min] = cell.value
+				
+			elif (nro%3)==2 and cell.value != None:
+				lista[articulo][may] = cell.value
+				
+			nro +=1 
 	for k,v in lista.items():	
 		print(k,v, end="\n") 
+
 
 
 if __name__ == '__main__':
