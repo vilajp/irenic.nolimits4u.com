2f521c4a1736cf562435066d1e8e9168ba298827
vilajp <vilajp@gmail.com>
2021-01-26 22:21:28 -0300

    se modifico para preveer que la planilla tenga habilitadas casillas fuera de los datos

--- listas.py
+++ listas.py
@@ -1,9 +1,10 @@
 import openpyxl
 import sys
+import os
 
 def main():
 	try:
-		wb_obj = openpyxl.load_workbook("sentida\\LISTA DE PRECIOS OCTUBRE 2020 excel.xlsx")
+		wb_obj = openpyxl.load_workbook("sentida\\LISTA DE PRECIOS ENERO 2021.xlsx")
 
 	except:
 		print("No se pudo abrir el archivo", sys.exc_info()[0])
@@ -13,26 +14,34 @@ def main():
 	print(sheet)
 	lista = {}
 	may = "Mayorista"
-	min = "Minorista"
+	mino = "Minorista"
 	nro = 0
 
 	for row in sheet.iter_rows():
 		
 		for cell in row:
+
 			if (nro%3)==0:
-				articulo = cell.value
-				lista[articulo] = {}
-				 
 				
-			elif (nro%3)==1 and cell.value != None:
-				lista[articulo][min] = cell.value
+					articulo = cell.value
+					lista[articulo] = {}
+
 				
-			elif (nro%3)==2 and cell.value != None:
+			elif (nro%3)==1 and cell.value != None and type(cell.value)!=str:
+				lista[articulo][mino] = cell.value
+				
+			elif (nro%3)==2 and cell.value != None and type(cell.value)!=str:
 				lista[articulo][may] = cell.value
-				
-			nro +=1 
-	for k,v in lista.items():	
-		print(k,v, end="\n") 
+			if nro < 9:	
+				nro +=1
+			else:
+				nro = 0 
+	for k,v in lista.items():
+		if v != {}:
+			try:
+				print(k,v, end="\n") 
+			except:
+				print(k,"no tiene precio minorista", end="\n")
 
 
 
