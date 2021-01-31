d26bcbd05af7b197d165aab93c39a33debc39ef2
vilajp <vilajp@gmail.com>
2021-01-31 14:23:38 -0300

    Se modifico listas.py para adaptarlo a los requerimientos pep por ejemplo espacios e identacion

--- listas.py
+++ listas.py
@@ -1,70 +1,67 @@
 import openpyxl
 import sys
-import os
+
 
 def main():
-	try:
-		wb_obj = openpyxl.load_workbook("sentida\\LISTA DE PRECIOS ENERO 2021.xlsx")
+    try:
+        wb_obj = openpyxl.load_workbook("sentida\\LISTA DE PRECIOS ENERO 2021.xlsx")
 
-	except:
-		print("No se pudo abrir el archivo", sys.exc_info()[0])
-		return
-	
-	sheet = wb_obj.active
-	print(sheet)
-	lista = {}
-	nro = 0
-	grupo = str(nro)
-	lista[grupo] = {}
+    except:
+        print("No se pudo abrir el archivo", sys.exc_info()[0])
+        return
 
-	for row in sheet.iter_rows():
-		
-		for cell in row:
+    sheet = wb_obj.active
+    print(sheet)
+    lista = {}
+    nro = 0
+    grupo = str(nro)
+    lista[grupo] = {}
 
-			if (nro%3)==0:
-					grupo = str(nro)
-					articulo = cell.value
-					try:	
-						lista[grupo][articulo] = {}
-					except:
-						lista[grupo] = {}
-						lista[grupo][articulo] = {}
+    for row in sheet.iter_rows():
 
-				
-			elif (nro%3)==1 and cell.value != None and type(cell.value)!=str:
-				lista[grupo][articulo]["min"] = cell.value
-				
-			elif (nro%3)==2 and cell.value != None and type(cell.value)!=str:
-				lista[grupo][articulo]["may"] = cell.value
-			try:
-				lista[grupo][articulo]["margen"] = round(100 * (lista[grupo][articulo]["min"] - \
-				lista[grupo][articulo]["may"]) / lista[grupo][articulo]["may"],2)
-			except:
-				next
+        for cell in row:
 
-			if nro < 9:	
-				nro +=1
-			else:
-				nro = 0 
-	tipos = {}
-	n_tipo = 0
-	
-	for k in lista.keys():
-		for q, v in lista[k].items():
-			if v=={}:
-			
-				tipo = q
-				n_tipo +=1
-				continue
-			
-			tipos[n_tipo] = tipo
-			v["tipo"] = n_tipo
-			
-				
-			
-			print(q,v)
+            if (nro % 3) == 0:
+                grupo = str(nro)
+                articulo = cell.value
+                try:
+                    lista[grupo][articulo] = {}
+                except:
+                    lista[grupo] = {}
+                    lista[grupo][articulo] = {}
 
-	print(tipos.items())
+            elif (nro % 3) == 1 and not None and type(cell.value) != str:
+                lista[grupo][articulo]["min"] = cell.value
+
+            elif (nro % 3) == 2 and not None and type(cell.value) != str:
+                lista[grupo][articulo]["may"] = cell.value
+            try:
+                lista[grupo][articulo]["margen"] = round(100 * (lista[grupo][articulo]["min"] - \
+                lista[grupo][articulo]["may"]) / lista[grupo][articulo]["may"], 2)
+            except:
+                next
+
+            if nro < 9:
+                nro += 1
+            else:
+                nro = 0
+    tipos = {}
+    n_tipo = 0
+
+    for k in lista.keys():
+        for q, v in lista[k].items():
+            if v == {}:
+                tipo = q
+                n_tipo += 1
+                continue
+
+            tipos[n_tipo] = tipo
+            v["tipo"] = n_tipo
+
+            print(q, v)
+
+    print(tipos.items())
+
 
 if __name__ == '__main__':
-	main()
+    main()
