f7eee12d5c33ad2bb26ef1fa6b05f1fcd522a343
vilajp <vilajp@gmail.com>
2021-01-31 21:39:41 -0300

    primer commit se pretende bajar los articulos y precios de shiri

---
+++ shirinatural.py
@@ -0,0 +1,73 @@
+import sqlite3
+import urllib.error
+import ssl
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+conn = sqlite3.connect('spider.sqlite')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS ProductosShiri
+    (id INTEGER PRIMARY KEY, nombre TEXT UNIQUE, precio INTEGER)''')
+
+url = "https://www.shirinatural.com.ar/productos/?mpage=3"
+
+try:
+    document = urlopen(url)#, context=ctx
+
+    html = document.read()
+
+    if document.getcode() != 200 :
+        print("Error on page: ",document.getcode())
+
+    if 'text/html' != document.info().get_content_type() :
+        print("Ignore non text/html page")
+
+    print('('+str(len(html))+')', end=' ')
+
+    soup = BeautifulSoup(html, "html.parser")
+except KeyboardInterrupt:
+    print('')
+    print('Program interrupted by user...')
+
+except:
+    print("Unable to retrieve or parse page")
+
+# Retrieve all of the div tags
+tags = soup('script')
+count = 0
+producto = {}
+for tag in tags:
+    tipo = tag.get("type", None)
+
+    if tipo == "application/ld+json":
+        print(tag)
+
+    # cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES ( ?, NULL, 1.0 )', ( href, ) )
+    # count = count + 1
+    # conn.commit()
+    #
+    # cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', ( href, ))
+    # try:
+    #     row = cur.fetchone()
+    #     toid = row[0]
+    # except:
+    #     print('Could not retrieve id')
+    #     continue
+    # # print fromid, toid
+    # cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES ( ?, ? )', ( fromid, toid ) )
+    #
+
+print(count)
+
+cur.close()
+
+
